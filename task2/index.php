b1512200c9cf427ef53761f886196eff1ede55a0
LotusEliza <lotuselizza@gmail.com>
2019-06-13 16:17:07 +0300

    home commit

---
+++ task2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'config.php';
+include 'autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+
+include 'templates/index.php';
---
+++ task2/libs/Cars.php
@@ -0,0 +1,26 @@
+<?php
+
+class Cars
+{
+
+    public function cars_list(){
+
+        //return id, brand mpdel
+    }
+
+    public function car_info($id){
+
+        //return <complex type>: year, capacity, color, speed, price
+    }
+
+    public function search_by_params($param){
+        
+    }
+
+    public function order($id, $name, $surname, $payment){
+
+        //return true false
+    }
+
+
+}
